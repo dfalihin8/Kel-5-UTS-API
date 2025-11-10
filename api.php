56e0c4082ed9732fff6b2eb5a74fea330d51bd96
dfalihin8 <151384235+dfalihin8@users.noreply.github.com>
2025-11-10 18:48:52 +0700

    Update api.php

--- api.php
+++ api.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
-// Route untuk login (tidak perlu auth)
+
 Route::post('/login', [AuthController::class, 'login']);
 
-// Route yang perlu authentication
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('mahasiswa', MahasiswaController::class);
 });
